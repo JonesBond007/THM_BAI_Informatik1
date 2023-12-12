b46428d30a3fa62753d438e402c9b1b800712d07
JonesBond007 <99079345+JonesBond007@users.noreply.github.com>
2023-12-12 19:52:14 +0100

    Add files via upload

---
+++ A52_Textzeile/A52_Textzeile/main.c
@@ -0,0 +1,36 @@
+/*
+	Project:	A52_Textzeile
+	File:		main.c
+	Author:		Jonas Fiegert
+	Date:		2023-11-29
+*/
+
+#define _CRT_SCECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+	char c;
+	char text[41];
+	int length = 40;
+	int i = 0;
+
+	printf("Texteingabe (max. %d Zeichen):\n", length);
+	
+	do {
+		c = getchar();
+		text[i] = c;
+		i++;
+	} while (c != '\n' && i <= length);
+
+	printf("\nTextausgabe:\n");
+
+	for (int j = 0; j < i; j++) {
+		printf("%c", text[j]);
+	}
+	
+	return 0;
+}
