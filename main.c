8135cc781ae1562a5bba6b0070b2db441cebfad8
JonesBond007 <99079345+JonesBond007@users.noreply.github.com>
2023-12-20 20:18:42 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+/*
+	Project:	A91_Leerzeichen
+	File:		main.c
+	Author:		Jonas Fiegert
+	Date:		2023-12-20
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_VALUES 250
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+
+/*----Funktionsdeklarationen----*/
+int countSpaces(char* text);
+
+/*----Hauptprogramm----*/
+int main() {
+	char text[MAX_VALUES];
+	int spaces;
+
+	printf("Texteingabe: \n");
+	gets(text);
+
+	spaces = countSpaces(text);
+	printf("Anzahl an Leerzeichen: %d\n", spaces);
+
+	return 0;
+}
+
+int countSpaces(char* text) {
+	int i, spaces;
+	spaces = 0;
+
+	for (i = 0; i < text[i] != '\0'; i++) {
+		if (text[i] == ' ') {
+			spaces++;
+		}
+	}
+
+	return spaces;
+}
