34e339a34339655bf9ec22c3ff6318aa59b9b6b3
JonesBond007 <99079345+JonesBond007@users.noreply.github.com>
2024-01-24 17:53:19 +0100

    Add files via upload

---
+++ Uebung04_Fehlersuche/Loesung04.c
@@ -0,0 +1,40 @@
+/*
+	Das Programm soll maximal 100 Spannungswerte einlesen und anschlieﬂend
+	den Mittelwert berechnen und ausgeben. Der Quelltext enth‰lt jedoch 6 Fehler.
+	Aufgabenstellung:
+	Korrigieren Sie die Fehler und laden Sie den korrigierten Quelltext
+	in Moodle hoch!
+*/
+
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+# define MAX 100
+
+int main()
+{
+	int i, anzahl = 0;
+	double daten[MAX], summe = 0, mittelwert;
+
+	printf("Maximal %d Spannungswerte eingeben, Ende mit negativem Wert:\n", MAX);
+	for (i = 0; i < MAX; i++)
+	{
+		scanf("%lf", &daten[i]);
+
+		if (daten[i] < 0) {
+			break;
+		}
+		summe += daten[i];
+		anzahl++;
+	}
+
+	if (anzahl == 0) {
+		printf("keine Werte!\n");
+		return 0;
+	}
+
+	mittelwert = summe / anzahl;
+	printf("Mittelwert: %lf V\n", mittelwert);
+
+	return 0;
+}
